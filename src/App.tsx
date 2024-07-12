63ec3ab8711859a31dcd89f26d5987ba6cafe889
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2024-07-12 09:40:29 -0300

    chore: add react-router and lib SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
+import './global.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <HelmetProvider>
+      <Helmet
+        titleTemplate='%s | pizza.shop'
+      />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
